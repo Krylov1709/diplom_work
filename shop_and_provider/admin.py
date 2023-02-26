1259aee830d63d12feb5a7d7832586061d3056be
Крылов Алексей <krylov1709@yandex.ru>
2023-02-26 22:48:27 +0300

    Изменение в моделях и их отображение в админке

--- shop_and_provider/admin.py
+++ shop_and_provider/admin.py
@@ -1,26 +1,62 @@
 from django.contrib import admin
-from shop_and_provider.models import Provider, Product, Parameter, Category
+from shop_and_provider import models
 
 
-@admin.register(Provider)
+@admin.register(models.Provider)
 class ProviderAdmin(admin.ModelAdmin):
-    list_display = ['id', 'name', 'email']
-    list_editable = ['name', 'email']
+    list_display = ['name', 'email']
 
 
-@admin.register(Product)
-class ProductAdmin(admin.ModelAdmin):
-    list_display = ['id', 'title']
-    list_editable = ['title']
-
-
-@admin.register(Parameter)
+@admin.register(models.Parameter)
 class ParameterAdmin(admin.ModelAdmin):
-    list_display = ['id', 'title']
-    list_editable = ['title']
+    list_display = ['title']
 
 
-@admin.register(Category)
+@admin.register(models.Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ['id', 'title']
-    list_editable = ['title']
+    list_display = ['title']
+
+
+class ParameterInfoInline(admin.TabularInline):
+    model = models.ParameterInfo
+    extra = 0
+
+
+@admin.register(models.Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'category', 'created_add']
+    inlines = [ParameterInfoInline]
+
+
+@admin.register(models.ProductProvider)
+class ProductProviderAdmin(admin.ModelAdmin):
+    list_display = [
+        'id',
+        'product',
+        'provider',
+        'price',
+        'status',
+        'created_add'
+    ]
+
+
+@admin.register(models.Shop)
+class ShopAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+
+@admin.register(models.Manager)
+class ManagerAdmin(admin.ModelAdmin):
+    list_display = ['name', 'email', 'shop']
+
+
+class OrderPositionInline(admin.TabularInline):
+    model = models.OrderPosition
+    extra = 0
+
+
+@admin.register(models.Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'number', 'manager', 'status', 'created_add']
+    list_editable = ['status']
+    inlines = [OrderPositionInline]
--- shop_and_provider/models.py
+++ shop_and_provider/models.py
@@ -22,14 +22,8 @@ class Provider(models.Model):
         verbose_name = "Поставщик"
         verbose_name_plural = "Поставщики"
 
-
-class Product(models.Model):
-    """Товар"""
-    title = models.CharField(max_length=100, verbose_name="Название")
-
-    class Meta:
-        verbose_name = "Товар"
-        verbose_name_plural = "Товары"
+    def __str__(self):
+        return self.name
 
 
 class Category(models.Model):
@@ -40,6 +34,9 @@ class Category(models.Model):
         verbose_name = "Категория"
         verbose_name_plural = "Категории"
 
+    def __str__(self):
+        return self.title
+
 
 class Parameter(models.Model):
     """Характеристика"""
@@ -49,51 +46,40 @@ class Parameter(models.Model):
         verbose_name = "Характеристика"
         verbose_name_plural = "Характеристики"
 
+    def __str__(self):
+        return self.title
 
-class ProductInfo(models.Model):
-    """Информация о товаре"""
-    provider = models.ForeignKey(
-        Provider,
-        on_delete=models.CASCADE,
-        verbose_name="Поставщик",
-        related_name="products_info"
-    )
-    product = models.ForeignKey(
-        Product,
-        on_delete=models.CASCADE,
-        verbose_name="Товар",
-        related_name="products_info"
+
+class Product(models.Model):
+    """Товар"""
+    title = models.CharField(
+        unique=True,
+        max_length=100,
+        verbose_name="Название"
     )
     category = models.ForeignKey(
         Category,
         on_delete=models.CASCADE,
         verbose_name="Категория",
-        related_name="products_info"
+        related_name="products"
     )
     parameters_info = models.ManyToManyField(
         Parameter,
         verbose_name="Характеристика",
-        related_name="products_info",
+        related_name="products",
         through="ParameterInfo"
-    )
-    price = models.PositiveIntegerField(
-        verbose_name="Стоимость"
     )
     created_add = models.DateTimeField(
         auto_now_add=True,
         verbose_name="Время создания"
     )
 
-    status = models.CharField(
-        max_length=10,
-        verbose_name="Статус",
-        choices=STATUS_PRODUCT,
-        default="CLOSED"
-    )
-
     class Meta:
-        verbose_name = "Информация о товаре"
-        verbose_name_plural = "Информация о товарах"
+        verbose_name = "Товар"
+        verbose_name_plural = "Товары"
+
+    def __str__(self):
+        return self.title
 
 
 class ParameterInfo(models.Model):
@@ -103,9 +89,9 @@ class ParameterInfo(models.Model):
         verbose_name="Характеристика",
         on_delete=models.CASCADE
     )
-    product_info = models.ForeignKey(
-        ProductInfo,
-        verbose_name="Информация о товаре",
+    product = models.ForeignKey(
+        Product,
+        verbose_name="Товар",
         on_delete=models.CASCADE
     )
     description = models.TextField(verbose_name="Описание")
@@ -115,6 +101,42 @@ class ParameterInfo(models.Model):
         verbose_name_plural = "Описания характеристик"
 
 
+class ProductProvider(models.Model):
+    """Товар поставщика"""
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE,
+        verbose_name="Товар",
+        related_name="providers"
+    )
+    provider = models.ForeignKey(
+        Provider,
+        on_delete=models.CASCADE,
+        verbose_name="Поставщик",
+        related_name="products"
+    )
+    price = models.PositiveIntegerField(
+        verbose_name="Стоимость"
+    )
+    status = models.CharField(
+        max_length=10,
+        verbose_name="Статус",
+        choices=STATUS_PRODUCT,
+        default="CLOSED"
+    )
+    created_add = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name="Время создания"
+    )
+
+    class Meta:
+        verbose_name = "Товар поставщика"
+        verbose_name_plural = "Товары постащиков"
+
+    def __str__(self):
+        return f'{self.product.title} ({self.provider.name})'
+
+
 class Shop(models.Model):
     """Магазин"""
     name = models.CharField(max_length=50, verbose_name="Название")
@@ -122,6 +144,9 @@ class Shop(models.Model):
     class Meta:
         verbose_name = "Магазин"
         verbose_name_plural = "Магазины"
+
+    def __str__(self):
+        return self.name
 
 
 class Manager(models.Model):
@@ -140,9 +165,16 @@ class Manager(models.Model):
         verbose_name = "Менеджер"
         verbose_name_plural = "Менеджеры"
 
+    def __str__(self):
+        return self.name
+
 
 class Order(models.Model):
     """Заказ"""
+    number = models.PositiveIntegerField(
+        verbose_name="Номер",
+        unique=True
+    )
     manager = models.ForeignKey(
         Manager,
         verbose_name="Менеджер",
@@ -155,9 +187,9 @@ class Order(models.Model):
         choices=STATUS_ORDER,
         default="NEW"
     )
-    products_info = models.ManyToManyField(
-        ProductInfo,
-        verbose_name="Информация о товаре",
+    products_provider = models.ManyToManyField(
+        ProductProvider,
+        verbose_name="Товары",
         through='OrderPosition',
         related_name="orders"
     )
@@ -170,6 +202,9 @@ class Order(models.Model):
         verbose_name = "Заказ"
         verbose_name_plural = "Заказы"
 
+    # def __str__(self):
+    #     return self.id
+
 
 class OrderPosition(models.Model):
     """Позиция в заказе"""
@@ -178,9 +213,9 @@ class OrderPosition(models.Model):
         verbose_name="Заказ",
         on_delete=models.CASCADE,
     )
-    product_info = models.ForeignKey(
-        ProductInfo,
-        verbose_name="Информация о товаре",
+    product_provider = models.ForeignKey(
+        ProductProvider,
+        verbose_name="Товар",
         on_delete=models.CASCADE,
     )
     quantity = models.PositiveIntegerField(
@@ -192,3 +227,4 @@ class OrderPosition(models.Model):
     class Meta:
         verbose_name = "Позиция в заказе"
         verbose_name_plural = "Позиции в заказе"
+
